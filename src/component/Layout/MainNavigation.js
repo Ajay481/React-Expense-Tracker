07959992e717b5f0b139209699bc43d55c7ff393
Ajay481 <iamajayyadav481@gmail.com>
2023-03-07 12:27:10 +0530

    Email Verification Added

--- src/component/Layout/MainNavigation.js
+++ src/component/Layout/MainNavigation.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../store/AuthContext";
 import classes from "./MainNavigation.module.css";
 
 export const MainNavigation = () => {
+  const authCtx = useContext(AuthContext)
+  const isLoggedIn = authCtx.isLoggedIn
   return (
     <div className={classes.head}>
       <header className={classes.header}>
-        <h1>MyWebBlog</h1>
-        <NavLink to="/home">Home</NavLink>
-        <h3>Products</h3>
-        <NavLink to="/profile">Profile</NavLink>
-        <h3>About Us</h3>
+        {isLoggedIn && <h1>MyWebBlog</h1>}
+        {isLoggedIn && <NavLink to="/home">Home</NavLink>}
+        {isLoggedIn && <h3>Products</h3>}
+        {isLoggedIn && <NavLink to="/profile">Profile</NavLink>}
+        {isLoggedIn && <h3>About Us</h3>}
       </header>
     </div>
   );
--- src/component/Layout/StartingPage.js
+++ src/component/Layout/StartingPage.js
@@ -1,19 +1,55 @@
+import { useContext } from "react";
 import { useHistory } from "react-router";
+import { AuthContext } from "../store/AuthContext";
 
 import classes from "./StartingPage.module.css";
 
 export const StartingPage = () => {
+  const authCtx = useContext(AuthContext);
   const history = useHistory();
+
+  const verifyHandler = () => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBlvwm2UFysqlxp549MzHN_mTVXIn57d7s",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: authCtx.token,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errMessage = "Authentication Failed";
+            throw new Error(errMessage);
+          });
+        }
+      })
+      .then((data) => console.log(data))
+      .catch((err) => alert(err.message));
+  };
 
   const profileHandler = () => {
     history.replace("/profile");
   };
   return (
-    <div className={classes.welcome}>
-      <div>Welcome to Expense Tracker!!!</div>
-      <div>
-        <span>Your Profile is Incomplete.</span>
-        <button onClick={profileHandler}>Complete Now.</button>
+    <div>
+      <div className={classes.welcome}>
+        <div>Welcome to Expense Tracker!!!</div>
+        <div>
+          <span>Your Profile is Incomplete.</span>
+          <button onClick={profileHandler}>Complete Now.</button>
+        </div>
+      </div>
+      <div className={classes.btn}>
+        <button onClick={verifyHandler}>VERIFY EMAIL ID</button>
       </div>
     </div>
   );
